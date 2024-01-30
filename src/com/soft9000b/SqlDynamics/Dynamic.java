e31b869027b518fb485e0303b88e192ecdfdb8ae
Randall Nagy <randall.nagy@soft9000.com>
2024-01-30 02:40:41 -0500

    Dynamic column detection.

--- src/com/soft9000b/SqlDynamics/Dynamic.java
+++ src/com/soft9000b/SqlDynamics/Dynamic.java
@@ -6,6 +6,7 @@
 package com.soft9000b.SqlDynamics;
 
 import com.soft9000.SQLMate.SqlColumn;
+import com.soft9000b.tv.TagPair;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
@@ -14,21 +15,40 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ * Smart & sparce SQL Query results. Can dynamically detect "as" fields.
+ *
+ * @author Randall Nagy
+ */
 public class Dynamic {
 
-    public static ArrayList<ArrayList<DynamicPair>> SelectFields(Connection conn, String sqlSelect, List<SqlColumn> fields) {
+    /**
+     * Detect & select results from an SQL Select.
+     *
+     * @param conn An active database connection.
+     * @param sqlSelect The SQL Select statement. Can detect "as" fields.
+     * @param fields Dynamic list of field names therein.
+     * @return All rows, as detected.
+     */
+    public static ArrayList<ArrayList<TagPair>> SelectFields(Connection conn, String sqlSelect, List<SqlColumn> fields) {
         try {
             if (conn == null) {
                 throw new Exception("Connection Object Reference is NULL.");
             }
 
+            ArrayList<String> asIf = getDynamicFileds(sqlSelect);
+
             Statement smt = conn.createStatement();
             ResultSet ref = smt.executeQuery(sqlSelect);
-            ArrayList<ArrayList<DynamicPair>> results = new ArrayList<>();
+            ArrayList<ArrayList<TagPair>> results = new ArrayList<>();
             while (ref.next()) {
-                ArrayList<DynamicPair> row = new ArrayList();
+                ArrayList<TagPair> row = new ArrayList();
+                for (String dy : asIf) {
+                    getRow(row, ref, dy);
+                    results.add(row);
+                }
                 for (SqlColumn field : fields) {
-                    addIff(row, ref, field.getColumnName());
+                    getRow(row, ref, field.getColumnName());
                     results.add(row);
                 }
             }
@@ -40,14 +60,29 @@ public class Dynamic {
         }
     }
 
-    static void addIff(ArrayList<DynamicPair> results, ResultSet ref, String ColumnName) {
-        DynamicPair tv = new DynamicPair();
-        if (addIff(tv, ref, ColumnName)) {
+    /**
+     * Row definition as-a detection operation.
+     *
+     * @param results List of detected files names & values.
+     * @param ref The SQL query response.
+     * @param ColumnName The field / column name to check for.
+     */
+    static void getRow(ArrayList<TagPair> results, ResultSet ref, String ColumnName) {
+        TagPair tv = new TagPair();
+        if (getColumn(tv, ref, ColumnName)) {
             results.add(tv);
         }
     }
 
-    static boolean addIff(DynamicPair result, ResultSet ref, String ColumnName) {
+    /**
+     * Populate TagPair if, and only if, the column-name is present.
+     *
+     * @param result A column name + value pair, if present.
+     * @param ref The SQL query response.
+     * @param ColumnName The field / column name to check for.
+     * @return True when the field name is found in the result set.
+     */
+    static boolean getColumn(TagPair result, ResultSet ref, String ColumnName) {
         try {
             int pos = ref.findColumn(ColumnName);
             result.setTag(ColumnName);
@@ -55,6 +90,34 @@ public class Dynamic {
             return true;
         } catch (Exception ex) {
             return false;
+        }
+    }
+
+    /**
+     * Detect any dynamic fields.
+     *
+     * @param sqlSelect
+     * @return The effective database field definition.
+     */
+    public static ArrayList<String> getDynamicFileds(String sqlSelect) {
+        ArrayList<String> results = new ArrayList<String>();
+        String[] cols = sqlSelect.replaceAll(",", "").replaceAll(";", "").toLowerCase().split(" as ");
+        boolean bskip = true;
+        for (String col : cols) {
+            if (!bskip) {
+                String[] asif = col.split(" ");
+                if (asif.length >= 1) {
+                    results.add(asif[0]);
+                }
+            }
+            bskip = false;
+        }
+        return results;
+    }
+
+    public static void main(String[] args) {
+        for (String val : getDynamicFileds("select foo as bar, select bar as net;")) {
+            System.out.println(val);
         }
     }
 
